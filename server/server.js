c744fd7df195b799e96c25cb0e21e5e3fb73a2a5
shoeb512 <125538271+shoeb4dev@users.noreply.github.com>
2026-08-20 15:23:42 +0530

    Added change

--- server/server.js
+++ server/server.js
@@ -2,18 +2,25 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/db");
 
 const app = express();
 
+// Connect to MongoDB
+connectDB();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Test Route
 app.get("/", (req, res) => {
   res.json({
     message: "Travora API is running 🚀"
   });
 });
 
+// Server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
